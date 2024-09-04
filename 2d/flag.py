2b86977d35bc060695fc7a6ed64016c6ea60fb2f
ediree0 <ediree0@gmail.com>
2024-09-04 19:55:12 +0900

    Solved 2d

---
+++ 2d/flag.py
@@ -0,0 +1,17 @@
+import binascii
+
+def main(): 
+	with open("2d.txt",'r') as file:
+		text = file.read()
+		binary = ''
+		for _ in text:
+			if _ == '1' or _ == '0':
+				binary += _
+	file.close()
+	n = int(binary,2)
+	flag = n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+	print(flag)
+
+
+if __name__ == '__main__':
+	main()
