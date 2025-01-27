85eb320ad1b4f63a637e52ea8dfd5a40bc934670
ediree0 <ediree0@gmail.com>
2025-01-27 19:08:59 +0900

    Programming > Prime (No Writeup)

---
+++ programming/prime/solve.py
@@ -0,0 +1,14 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+a=0
+for number in range(2, 1338):
+    if is_prime(number):
+        a+=number
+
+print(a)
