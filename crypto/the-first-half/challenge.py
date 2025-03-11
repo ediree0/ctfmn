cf062a28b802bd3a7403a9013d47281719303456
ediree0 <ediree0@gmail.com>
2025-03-11 14:23:18 +0900

    Crypto > The First Half (Exploit payload)

--- crypto/the-first-half/challenge.py
+++ crypto/the-first-half/challenge.py
@@ -2,27 +2,15 @@ import os
 
 flag = os.environ.get('flag', 'MUSTCTF{fake_flag_for_testing}').encode()
 
-key = sum([c for c in b'MUSTCTF']) # 550
-
 def leak(i):
-    return flag[i] ^ key
+    return flag[i] ^ sum([c for c in b'MUSTCTF'])
 
-def find_flag():
-    flag = ""
-    for i in range(-1,len(flag) * -1, -1):
-        flag += first_function(i)
 
-    flag = flag[::-1]
-    flag = "".join(flag)
-
-    print(flag)
-
-def first_function(input):
+while True:
     try:
-        i = int(input)
+        i = int(input('i = '))
         assert i < len(flag) // 2  # Sorry. Only first half is leaked
-        return chr(leak(i)^key)
+        print('Leak:', leak(i))
     except Exception:
         print('Unexpected error')
-
-find_flag()
+        break
