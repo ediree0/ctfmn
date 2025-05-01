e13d9edd91ce509cc994db2010277eeaf02d41a7
ediree0 <ediree0@gmail.com>
2025-05-01 20:38:16 +0900

    Ghost in the Cybergrid

---
+++ reverse/ghost-in-the-cybergrid/solve.py
@@ -0,0 +1,13 @@
+with open("secret.bin", "rb") as f:
+    data = f.read()
+f.close()
+
+key = [0x13, 0x37, 0x56, 0x78]
+
+flag = ""
+for i in range(len(data)):
+    flag += chr(data[i] ^ key[i % 4])
+
+print(flag)
+
+# MUSTCTF{SP3CTR_S3E_ALL}
