849117795c6c89fe91b4020696c84950b817284e
ediree0 <ediree0@gmail.com>
2025-09-18 12:55:42 +0900

    Deletion > Misc > log

--- misc/log/extract.py
+++
@@ -1,8 +0,0 @@
-with open("numbers.log", "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    parts = line.split("GET /")
-    if len(parts) > 1: 
-        char = parts[1][0]  
-        print(ord(char))
